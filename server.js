cedcbf9d5a1f39092a7d9ce1d9602fdc84e8d82c
ortab123 <131905006+ortab123@users.noreply.github.com>
2025-08-31 13:11:47 +0300

    Add morgan to the project add remove logger middleware

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require("express");
-const { logger } = require("./middlewares/logger");
+const morgan = require("morgan");
 const { errorHandler } = require("./middlewares/errorHandler");
 const recipesRouter = require("./routes/recipes");
 
 const app = express();
 const PORT = 3000;
 
-// JSON middleware
 app.use(express.json());
 
-// Logger middleware
-app.use(logger);
+app.use(morgan("combined"));
 
-// Routes
 app.use("/api/recipes", recipesRouter);
 
-// Error handling middleware
 app.use(errorHandler);
 
-// Start server
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
