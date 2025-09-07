82b2828209d9043eb30ff443fac749a7bec5ac26
ortab123 <131905006+ortab123@users.noreply.github.com>
2025-09-07 18:12:39 +0300

    fixed model file names case

--- config/database.js
+++ config/database.js
@@ -1,8 +1,19 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("recipes_db", "", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    })
+  : new Sequelize("recipes_db", "", "", {
+      host: "localhost",
+      dialect: "mysql",
+    });
 
 module.exports = sequelize;
